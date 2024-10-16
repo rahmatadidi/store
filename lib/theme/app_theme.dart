d783e92f92a035b5af7f2ea15ab98513c48db1f8
RAS <user@MacBook.local>
2024-10-16 21:42:14 +0700

    feat: theme

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primaryColor = Color.fromRGBO(20, 20, 20, 1.0);
+  static const Color accentColor = Color.fromRGBO(53, 158, 253, 1.0);
+  static const Color secondaryColor = Color.fromRGBO(38, 48, 59, 1.0);
+  static const Color textColorPrimary = Colors.white;
+  static const Color textColorAccent = Color.fromRGBO(156, 171, 186, 1.0);
+}
